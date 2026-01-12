5a5182ed5d940f890d703ac6d609c2a44feb0797
GitGudKid24 <ryankahlie2001@gmail.com>
2026-01-11 23:43:14 -0800

    Add files via upload

---
+++ KahlieRyanPythonWeekOneAssignment.py
@@ -0,0 +1,46 @@
+#setting variables for use with f-strings
+from calendar import monthcalendar, firstweekday
+
+sibling = "sister"
+sibling_name = "Christina"
+#calling variables in a string
+print(f"I have a {sibling} and her name is {sibling_name}")
+
+#setting up more variables
+name = input("Enter your first name: ")
+age = input("Enter your current age: ")
+savings = input("Enter your yearly savings: $ ")
+
+#using integers in the called variables to modify them without using a function
+print(f"Hello {name}! You are currently {age} years old. In 10 years you will be {int(age) + 10} years old.")
+print(f" If you save ${savings} each year, in 5 years you will have saved {int(savings)*5}.")
+print(f"Your average monthly savings is {int(savings)/12} dollars per month.")
+
+#importing calendar to calculate how many days in a month
+import calendar
+
+def seconds_in_month():
+    # Prompt the user for input on the month
+    mth = input("Enter the month by number: ")
+    #converts to integer
+    month_number = int(mth)
+    _, number_of_days = calendar.monthrange(2026, month_number)
+    #calculating the number of seconds per day
+    total_seconds = number_of_days * 86400
+    print(f"There are {total_seconds} seconds in month {month_number} (which has {number_of_days} days).")
+#even after looking up all of this it took me forever to figure out especially to use the underscore for a throwaway variable
+    return total_seconds
+
+# Calls the function
+seconds_in_month()
+
+def full_cartons_of_eggs():
+    eggs = input("How many eggs do you have? :")
+    eggs_number = int(eggs) #converts to integer
+    cartons = eggs_number // 12 #divides by 12 and finds remainder after whole number divisions respectively
+    remainder = eggs_number % 12 #using helpful tips from homework for the remainder
+    print(f"There are {cartons} cartons of eggs with {remainder} remaining.") #f function to print with variables
+
+full_cartons_of_eggs()
+
+
